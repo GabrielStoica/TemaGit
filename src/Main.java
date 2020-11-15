937ac03ec0c4200d09cad2099f4f47f1a653ceb9
AlexP <alex.pokharel@gmail.com>
2020-11-15 14:00:08 +0200

    adaugare lista elevi
    am adaugat lista de elevi in main si 2 elevi cu toate datele
    
    Signed-off-by: AlexP <alex.pokharel@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
 
+        elev elev1 = new elev("Ion", "Popescu", 17, "a XI-a I");
+        elev elev2 = new elev("Andeea", "Georgescu", 18, "a XII-a J");
+
+        elev1.print_info();
+        elev2.print_info();
+
+        ArrayList<elev> lista_elevi = new ArrayList<elev>();
+        lista_elevi.add(elev1);
+        lista_elevi.add(elev2);
+
+        
     }
 }
