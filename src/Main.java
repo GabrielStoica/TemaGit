c072436a730379890b0f14f1fe834a6af2864d57
AlexP <alex.pokharel@gmail.com>
2020-11-15 15:24:31 +0200

    modificare request comment git
    am adaugat un elev, dupa cum ai cerut in git comment
    
    Signed-off-by: AlexP <alex.pokharel@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,17 @@ public class Main {
 
         elev elev1 = new elev("Ion", "Popescu", 17, "a XI-a I");
         elev elev2 = new elev("Andeea", "Georgescu", 18, "a XII-a J");
+        elev elev3 = new elev("Gabi", "Stoica", 18, "a XII-a J");
 
         elev1.print_info();
         elev2.print_info();
+        elev3.print_info();
 
         ArrayList<elev> lista_elevi = new ArrayList<elev>();
         lista_elevi.add(elev1);
         lista_elevi.add(elev2);
+        lista_elevi.add(elev3);
 
-        
+
     }
 }
