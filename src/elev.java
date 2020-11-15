22f7de1da4672f2e92a08b3cfb4d052a4b28d276
AlexP <alex.pokharel@gmail.com>
2020-11-15 13:42:55 +0200

    adaugare clasa elev + constructor
    am adaugat clasa elav si am creat constructor cu 4 paramatri
    
    Signed-off-by: AlexP <alex.pokharel@gmail.com>

---
+++ src/elev.java
@@ -0,0 +1,14 @@
+public class elev {
+
+    private String nume;
+    private String prenume;
+    private int varsta;
+    private String clasa;
+
+    public elev(String nume, String prenume, int varsta, String clasa) {
+        this.nume = nume;
+        this.prenume = prenume;
+        this.varsta = varsta;
+        this.clasa = clasa;
+    }
+}
