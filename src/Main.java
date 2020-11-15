4b2d853ffe2f60a99c3fc75444f937e2e54931f2
marius-gabriel <marius_gabriel1998@yahoo.com>
2020-11-15 14:37:51 +0200

    Instantiere clasa Scoala in clasa Main
    Am instantiat un obiect de tip Scoala in clasa Main, si l-am initializat cu lista de elevi existenta
    
    Signed-off-by: marius-gabriel <marius_gabriel1998@yahoo.com>

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args){
 
         Scoala scoala_Baltazar = new Scoala("Scoala Gimnaziala nr.1 Baltazar", "Stiubeiului nr.28 Buzau", "Gimnaziala", lista_elevi);
+        System.out.println(scoala_Baltazar);
 
     }
 }
