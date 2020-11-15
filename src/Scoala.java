129d558c4c760c8704a90b5020983de901846e2d
marius-gabriel <marius_gabriel1998@yahoo.com>
2020-11-15 13:51:23 +0200

    Creare constructor pentru clasa Scoala
    Am creat constructorul pentru cei 4 parametrii ai clasei Scoala
    
    Signed-off-by: marius-gabriel <marius_gabriel1998@yahoo.com>

--- src/Scoala.java
+++ src/Scoala.java
@@ -3,8 +3,17 @@ import java.util.Collections;
 
 public class Scoala {
 
+    private String nume;
+    private String adresa;
+    private String tip;
     private ArrayList<Elev> elevi_gimnaziu;
-    
+
+    public Scoala(String nume, String adresa, String tip, ArrayList<Elev> elevi_gimnaziu) {
+        this.nume = nume;
+        this.adresa = adresa;
+        this.tip = tip;
+        this.elevi_gimnaziu = elevi_gimnaziu;
+    }
 }
 
 
